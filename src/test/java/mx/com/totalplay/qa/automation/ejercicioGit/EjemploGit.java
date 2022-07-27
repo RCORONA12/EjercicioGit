bd71eb320b9f7eb2c8abeca4ef995c5fe0d1d483
Ricardo Corona <rcoronar@gmail.com>
2022-07-27 11:02:35 -0500

    Add two activitys and desing the connection beetween them

---
+++ src/test/java/mx/com/totalplay/qa/automation/ejercicioGit/EjemploGit.java
@@ -0,0 +1,9 @@
+package mx.com.totalplay.qa.automation.ejercicioGit;
+
+public class EjemploGit {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+
+    }
+}
